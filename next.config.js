dfa666b97a2d5397031d0d25f55d1af285d3f59f
Ethan Pollack <epollack31@gmail.com>
2022-12-10 02:18:25 -0500

    save

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace('/.*?\//', '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -6,7 +18,9 @@ const nextConfig = {
 		fcdb_uri: 'mongodb+srv://fcdb_sa:fcdb_sa@fcdb-cluster.6xlhjgz.mongodb.net/?retryWrites=true&w=majority',
 		fcdb_user: 'fcdb_sa',
 		fcdb_pass: 'fcdb_sa'
-	}
+	},
+	assetPrefix: assetPrefix,
+  basePath: basePath,
 }
 
 module.exports = nextConfig;
